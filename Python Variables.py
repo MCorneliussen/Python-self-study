572ea3663d4a974a85331f02c70eafc37262338f
CopyrightNOR <80279903+CopyrightNOR@users.noreply.github.com>
2022-10-06 11:41:41 +0200

    Create Python Variables.py

---
+++ Python Variables.py
@@ -0,0 +1,19 @@
+# variables store data.
+
+# these are string variables. They can be declared a string by wrapping the value inside of a single or double quotatation mark.
+
+variable_one = 'hello' # you can seperate the variable to the equals symbol and the string
+variable_02="world!" # but it is not necassary.
+
+# you can not name a variable starting with a number. E.g. "1variable_example"
+# you can use underscore to seperate words but you can not use space.
+print(variable_one,variable_02,"\n")
+variable_02 = 'world!'
+
+print(variable_one, variable_02)
+
+# Variable names are case-sensitive. 
+Example_variable = 1
+example_variable = 2
+
+# Will be two different variables, storing their own data and be seperate of eachother
