10c63ed45f8a34433040adc303ccc0e7487fa0da
CopyrightNOR <80279903+CopyrightNOR@users.noreply.github.com>
2022-10-06 10:50:34 +0200

    Create Python Data Types.py

---
+++ Python Data Types.py
@@ -0,0 +1,18 @@
+# Data Types for Python
+
+# Int (Integer) = Whole numbers, positive or negative numbers without decimals of unlimited length. 
+# E.g. 98412531631123, -12 or +51613 (the + symbol is redundant and not necessary to define a number being positive or denoting addition, but it can be used in Python.)
+
+# Float = Numbers with decimals are floats and adds another level of accuracy.
+# E.g. 123.0, -123.0 or +123.0
+
+# String = Arrays of bytes representing unicode characters. Anything surrounded by single or double quotation marks.
+# Wrap the string in a different quotation mark to add the opposite in the text or use backslack \ aka escape character
+'example'
+"another"
+'"one more example"'
+'It\'s pretty windy outside.'
+
+# Boolean values
+True
+False
