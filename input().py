a91d7809faaaddfde1f149c7938b13aa2f89d8e9
CopyrightNOR <80279903+CopyrightNOR@users.noreply.github.com>
2022-10-06 11:54:56 +0200

    Create input().py

---
+++ input().py
@@ -0,0 +1,9 @@
+# input()
+
+# Python stops executing when it comes down to the input() function and continues when the user has submitted some input.
+
+name = input('Name: ') # the user types in a name and the value is stored in the variable.
+age = input('Age: ') # you can have multiple inputs.
+
+# after the user has input data the print function is called and will print out whatever was input.
+print('Hello', name, 'you are', age, 'years old.') 
