908a0a97e900bdb04e25211d176a4019f6a055d2
CopyrightNOR <80279903+CopyrightNOR@users.noreply.github.com>
2022-10-06 11:20:08 +0200

    Create print.py

---
+++ print.py
@@ -0,0 +1,12 @@
+# print() = is a built in function
+print('Hello World!') #This will print out the message "Hello World!" without quotation marks, singular or double.
+
+# examples of invalid prints and the reasons why they are invalid.
+
+# print(elephants)    #the computer does not know how to interpret plain text that is not wrapped in quotation marks and it will give you a NameError and say 'elephants' is not defined.
+# print("invalid')    #quotation marks that start and end a string has to be corresponding. E.g. "Example 1", 'Example two'.
+# Print("What")       #Print is not defined, It is a lowercase p. Print is not defined. The function is print.
+
+
+print(1.1,"is a 'Float'","and",2,'is an "Integer"',True)
+# the message can be a string or any other object and it will be converted into a string before printed on your screen.
