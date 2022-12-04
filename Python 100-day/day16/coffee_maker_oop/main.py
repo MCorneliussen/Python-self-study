8b0a853e98f4aadcadc7545a0337bb11d608f8d3
CopyrightNOR <Fujica94@gmail.com>
2022-12-04 18:46:03 +0100

    yep

---
+++ Python 100-day/day16/coffee_maker_oop/main.py
@@ -0,0 +1,9 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+print(Menu.get_items())
+
+#In progress
+
+#Review day 1- 16 and collect the useful things into another folder and clean up repository
